404297dd74ec83f3f05cc3f2045114f1de749b78
hyperdownload <89705058+hyperdownload@users.noreply.github.com>
2023-10-06 16:34:58 -0300

    v.0.1

--- script.js
+++ script.js
@@ -4,20 +4,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const prevButton = document.getElementById("prevButton");
     const nextButton = document.getElementById("nextButton");
-    const elementsPerPage = 8; 
-    let currentPage = 0; 
+    const elementsPerPage = 8;
+    let currentPage = 0;
 
     const elements = [
-        { name: "Elemento 1", link: "https://www.ejemplo.com/enlace1",image:"3ragen.png" },
-        { name: "Elemento 2", link: "https://www.ejemplo.com/enlace2",image:"3ragen.png"},
-        { name: "Elemento 3", link: "https://www.ejemplo.com/enlace3",image:"3ragen.png" },
-        { name: "Elemento 4", link: "https://www.ejemplo.com/enlace4",image:"3ragen.png" },
-        { name: "Elemento 5", link: "https://www.ejemplo.com/enlace5",image:"3ragen.png" },
-        { name: "Elemento 6", link: "https://www.ejemplo.com/enlace6",image:"3ragen.png" },
-        { name: "Elemento 7", link: "https://www.ejemplo.com/enlace7",image:"3ragen.png" },
-        { name: "Elemento 8", link: "https://www.ejemplo.com/enlace8",image:"3ragen.png" },
-        { name: "eri gay", link: "https://www.ejemplo.com/enlace8",image:"3ragen.png" },
+        { name: "Elemento 1", link: "https://www.ejemplo.com/enlace1", image: "3ragen.png" },
+        { name: "Elemento 2", link: "https://www.ejemplo.com/enlace2", image: "3ragen.png" },
+        { name: "Elemento 3", link: "https://www.ejemplo.com/enlace3", image: "3ragen.png" },
+        { name: "Elemento 4", link: "https://www.ejemplo.com/enlace4", image: "3ragen.png" },
+        { name: "Elemento 5", link: "https://www.ejemplo.com/enlace5", image: "3ragen.png" },
+        { name: "Elemento 6", link: "https://www.ejemplo.com/enlace6", image: "3ragen.png" },
+        { name: "Elemento 7", link: "https://www.ejemplo.com/enlace7", image: "3ragen.png" },
+        { name: "Elemento 8", link: "https://www.ejemplo.com/enlace8", image: "3ragen.png" },
+        { name: "Elemento 8", link: "https://www.ejemplo.com/enlace8", image: "3ragen.png" },
     ];
+
+    const carrito = [];
+    function showCantCarrito() {
+        const cantidadTotal = carrito.reduce((total, item) => total + item.cantidad, 0);
+        document.getElementById("cantidadCarrito").textContent = "Cantidad en el carrito: " + cantidadTotal;
+    }
 
     function showElements() {
         const searchTerm = searchInput.value.toLowerCase();
@@ -44,15 +50,37 @@ document.addEventListener("DOMContentLoaded", function () {
             text.textContent = element.name;
             div.appendChild(text);
 
-            div.addEventListener("click", () => {
-                window.location.href = element.link;
+            const cartButton = document.createElement("button");
+            cartButton.textContent = "Agregar al carrito";
+            div.appendChild(cartButton);
+
+            const buyButton = document.createElement("button");
+            buyButton.textContent = "Comprar";
+            div.appendChild(buyButton);
+
+            // Agregar evento para agregar al carrito
+            cartButton.addEventListener("click", () => {
+                addToCarrito(element);
             });
+
+
 
             elementContainer.appendChild(div);
         });
 
         prevButton.disabled = currentPage === 0;
         nextButton.disabled = endIndex >= filteredElements.length;
+    }
+
+    function addToCarrito(element) {
+        const productoExistente = carrito.find(item => item.nombre === element.name);
+        if (productoExistente) {
+            productoExistente.cantidad++;
+        } else {
+            carrito.push({ nombre: element.name, cantidad: 1 });
+        }
+        showCantCarrito(); // Actualiza la cantidad total
+        console.log("Producto agregado al carrito:", element.name);
     }
 
     prevButton.addEventListener("click", () => {
@@ -71,16 +99,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     searchInput.addEventListener("input", showElements);
-
+    showCantCarrito(); 
     showElements();
 });
-let segundos = 0;
-
-        // Función para actualizar el contador cada segundo
-        function actualizarContador() {
-            segundos++;
-            document.getElementById("contador").textContent = segundos + " segundos";
-        }
-
-        // Actualiza el contador cada 1000 milisegundos (1 segundo)
-        setInterval(actualizarContador, 1000);
